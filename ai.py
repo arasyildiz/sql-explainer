dd554db304eb8c57ad1c16fdec14937931096f2c
Aras Yildiz <aras@yordam.com>
2026-04-20 17:07:51 +0300

    fonksiyonel yapıya geçildi

--- ai.py
+++ ai.py
@@ -3,6 +3,20 @@ import streamlit as st
 from dotenv import load_dotenv
 import os
 
+def hataBas():
+    st.warning("boş veri göndermeyiniz.")
+
+def logla(istek, cevap):
+    st.session_state["gecmis"].append({"sorgu": istek, "cevap": cevap})
+
+def yaz(cevap):
+    st.write(cevap)
+
+def apiGonder(system, request):
+    response = api.messages.create(model="claude-haiku-4-5", max_tokens=2000, system= system ,messages=[{"role":"user","content": request}])   
+    return response.content[0].text
+
+
 load_dotenv()
 api_key = os.getenv("ANTHROPIC_API_KEY")
 api = anthropic.Anthropic(api_key=api_key)
@@ -13,16 +27,23 @@ if "gecmis" not in st.session_state:
 request = st.text_area("SQL girin")
 
 buton = st.button("Açıkla")
+optimizeButon = st.button("Sorguyu optimize et")
 
 if buton and request:
     with st.spinner("Açıklanıyor..."):
-        r = api.messages.create(model="claude-haiku-4-5", max_tokens=2000, system="SQL sorgusunun ne işe yaradığını türkçe bir şekilde açıkla",messages=[{"role":"user","content": request}])   
-    ai_response = r.content[0].text
-    st.write(ai_response)
-    st.session_state["gecmis"].append({"sorgu": request, "cevap": ai_response})
-    
+        ai_response = apiGonder("SQL sorgusunun ne işe yaradığını türkçe bir şekilde açıkla", request)
+    yaz(ai_response)
+    logla(request,ai_response)
 elif not request and buton:
-    st.warning("boş veri göndermeyiniz.")
+    hataBas()
+
+if request and optimizeButon:
+    with st.spinner("Optimize ediliyor..."):
+        optimizeResponse = apiGonder("SQL sorgusunun optimize et", request)
+    yaz(optimizeResponse)
+    logla(request,optimizeResponse)
+elif not request and optimizeButon:
+    hataBas()   
 
 
 for item in st.session_state["gecmis"]:
